72ba27ab7e134debad669f7917e76515479ed600
OrangeMotor11 <51335661+OrangeMotor11@users.noreply.github.com>
2020-03-20 22:44:04 -0600

    view for the general login
    The view for the general login was created.

---
+++ general_login/views/general_login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Mapple</title>
+    <link href="../styles/styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div class="background">
+        <div class="transbox">
+            <img src="../imgs/Mapple.png" alt="maple_logo" width="400px" height="400px">
+            <h2>LOG IN</h2>
+            <form action="" method="post" id="log_in_form">
+                <label for="user_name">User name: </label>
+                <input type="text" id="user_name">
+                &nbsp;&nbsp;&nbsp;
+                <label for="user_password">Password: </label>
+                <input type="password" id="user_password">
+            </form>
+            <br>
+            <button class="log_in_button" type="submit" form="log_in_form" style="vertical-align:middle"><span>Log in </span></button>
+            <br>
+            <br>
+            <br>
+        </div>
+      </div>
+</body>
+</html>
