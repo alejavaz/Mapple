9026f25248e7cabb14b6df4f78519ef8cafd126d
FlorentinoGB <florentino.beltrang@gmail.com>
2020-03-27 13:06:18 -0600

    Ordering files and folders in the branch task_register_page

--- views/task_register.php
+++ views/task_register.php
@@ -4,108 +4,86 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task Register Page</title>
-<<<<<<< HEAD
-    <link rel="shortcut icon" href="../img/Mapple.png">
-=======
     <link rel="shortcut icon" href="../imgs/Mapple.png">
->>>>>>> Admin_page
     <link rel="stylesheet" type="text/css" href="../css/styles.css">
 
 </head>
 <body>
     <header>
-<<<<<<< HEAD
-        <img src="../Mapple.png" alt="Mapple_Logo">
-=======
         <img src="../imgs/Mapple.png" alt="Mapple_Logo">
->>>>>>> Admin_page
         <h1>Task Register Module </h1>
     </header>
 
     <fieldset>
         <!--<legend> Task Register Page</legend>-->
         <form action="" methot="POST">
-            <section>
+            <div>
                 User email: <input type="email" name="user_email" /><p></p>
                 <p></p>
                 <p></p>            
-            </section>
-            <artticle>
-                <div>
-<<<<<<< HEAD
-                    Git Proyect: <input type="text" name="git_proyect" /><p></p>  
-                    Git Branch: <textarea name="git_branch" ></textarea><p></p>
-                    <p></p>   
-                    Task Status: <input type="status" name="task_status" /><p></p>
-=======
-                <form>
-                    <!-- Lista de selección -->
-                    GIT Proyect:
-                    <select name="git_proyect">
-                        <!-- Opciones de la lista -->
-                        <option value="GitProyect1">GIT Proyect #1</option>
-                        <option value="GitProyect2">GIT Proyect #2</option> <!-- Opción por defecto -->
-                        <option value="GitProyect3">GIT Proyect #3</option>
-                    </select>
-                    </form>
-                    <p></p>
-                    <p></p>
-                    <!--Git Proyect: <input type="text" name="git_proyect" /><p></p>-->  
-                    <div>
-                    Git Branch: <textarea name="git_branch" ></textarea><p></p>
-                    <p></p>
-                    </div>
-                    <form>
-                    <!-- Lista de selección -->
-                    Task status:
-                    <select name="status">
-                        <!-- Opciones de la lista -->
-                        <option value="inProgress">InProgress</option>
-                        <option value="finished" selected>Finished</option> <!-- Opción por defecto -->
-                        <option value="pending">Pending</option>
-                    </select>
-                    </form>   
-                    <!--Task Status: <input type="status" name="task_status" /><p></p>-->
->>>>>>> Admin_page
-                    <p></p>
-                    <p></p>
-                    Comments: <textarea name="comments" row="2"></textarea>
-                    <p></p>
-                </div>
+            </div>
+            
+            <section>
                 <artticle>
-                    <div>
-                        <div>
-                            <div>
-                                Start Time: <input type="datetime-local" name="start_time" /><p></p>
-                            </div>  
-                            <div>  
-                                End Time: <input type="datetime-local" name="end_time" /><p></p>
-                                <p></p>
-                                <p></p>
-                            </div>
-<<<<<<< HEAD
-                            <div>  
-                                Total Hours: <input type="number" name="total_hours" /><p></p>
-=======
-                            <div>
-                                <label for="quantity"></lavel>  
-                                Total Hours: <input type="number" name="total_hours" 
-                                min="0.5" max="5" step="0.5"/><p></p>
->>>>>>> Admin_page
-                                <p></p>
-                                <p></p>
-                            </div>
-                        </div>    
-                        <div class="btn_finalize">    
-                            <input type="button" onclick="alert('Finished task: Thanck you!')" 
-                            name="finalize" value="Finalize Task"/>                            
-                        </div>
-                    </div>
-                </artticle>
-            <artticle>
-        </form>
+                    <form>
+                        <!-- Lista de selección -->
+                        Git Proyect:
+                        <select name="git_proyect">
+                            <!-- Opciones de la lista -->
+                            <option value="GitProyect1">GIT Proyect #1</option>
+                            <option value="GitProyect2">GIT Proyect #2</option> <!-- Opción por defecto -->
+                            <option value="GitProyect3">GIT Proyect #3</option>
+                        </select>
 
-    
+                        <p></p>
+                        <p></p>
+                        <!--Git Proyect: <input type="text" name="git_proyect" /><p></p>-->  
+                        <div>
+                            Git Branch: <textarea name="git_branch" ></textarea><p></p>
+                            <p></p>
+                        </div>
+
+                        <article>
+                            <form>
+                                <!-- Lista de selección -->
+                                Task status:
+                                <select name="status">
+                                    <!-- Opciones de la lista -->
+                                    <option value="inProgress">InProgress</option>
+                                    <option value="finished" selected>Finished</option> <!-- Opción por defecto -->
+                                    <option value="pending">Pending</option>
+                                </select>
+
+                                <!--Task Status: <input type="status" name="task_status" /><p></p>-->
+                                <p></p>
+                                <p></p>
+                                Comments: <textarea name="comments" row="2"></textarea>
+                            </form>
+                        </article>
+                    </form>                                      
+                </artticle>
+                
+                <div>
+                    <form>
+                        Start Time: <input type="datetime-local" name="start_time" /><p></p>
+                                
+                        End Time: <input type="datetime-local" name="end_time" /><p></p>
+                        <p></p>
+                        <p></p>
+
+                        <label for="quantity"></lavel>  
+                        Total Hours: <input type="number" name="total_hours" 
+                        min="0.5" max="5" step="0.5"/>
+                            <p></p>
+                        <div class="btn_finalize">    
+                            <input type="submit" onclick="alert('Finished task: Thanck you!')" 
+                            name="finalize" value="Finalize_Task"/>                            
+                        </div>
+                    </form>
+                </div>
+            <section>                    
+        </form>
+    </fieldset>    
 </body>
 </html>
 
