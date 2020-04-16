04efdc834e0c74d7f17c595724d668d88f102e77
OrangeMotor11 <51335661+OrangeMotor11@users.noreply.github.com>
2020-04-16 14:38:13 -0500

    Adding my scripts
    Scripts for my views(general login, access denied, general report, team lead's page)

--- js/scripts.js
+++ js/scripts.js
@@ -53,7 +53,7 @@ function confirm_delete(deletebox) {
     }
 }
 
-function change_button(checkbx,button_id) {
+function change_button(checkbx, button_id) {
     var btn = document.getElementById(button_id);
     if (checkbx.checked == true) {
         btn.disabled = "";
@@ -68,4 +68,70 @@ function highlight_input(x) {
 
 function white_input(x) {
     x.style.background = "white";
+}
+
+/* ACCESS DENIED */
+
+function bigImg(x) {
+    x.style.height = "550px";
+    x.style.width = "550px";
+};
+
+function normalImg(x) {
+    x.style.height = "500px";
+    x.style.width = "500px";
+};
+
+function clickinner_denied(go_back_btn) {
+    location.href = '../general_login.php';
+};
+
+/* GENERAL REPORT */
+
+function clickinner(go_back_btn_gral) {
+    location.href = 'team_lead_page.php';
+};
+
+/* TEAM LEAD */
+
+function fill_evaluation_data(id) {
+    document.getElementById("id_evaluation").value = id;
+}
+
+function fill_evaluation_data2(hours) {
+    document.getElementById("registered_hours").value = hours;
+}
+
+function fill_report_data(id) {
+    document.getElementById("report_id").value = id;
+}
+
+function highlight_input(x) {
+    x.style.background = "#ffcc80";
+}
+
+function white_input(x) {
+    x.style.background = "white";
+}
+
+function selected_hours(message) {
+    Swal.fire({
+        position: 'center',
+        icon: 'warning',
+        title: '<b class="warning">Hey... </b>',
+        html: '<b class="popup">' + message + '</b>',
+        background: '#FFCC70',
+        confirmButtonColor: '#880000'
+    });
+}
+
+function selected_report(message) {
+    Swal.fire({
+        position: 'center',
+        icon: 'warning',
+        title: '<b class="warning">Hey... </b>',
+        html: '<b class="popup">' + message + '</b>',
+        background: '#FFCC70',
+        confirmButtonColor: '#880000'
+    });
 }
